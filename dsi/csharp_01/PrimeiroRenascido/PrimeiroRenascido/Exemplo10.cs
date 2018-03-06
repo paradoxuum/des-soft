22e0c345475c49b0e5248740eebf09fe33612e42
Eduardo Lima <haevorak@gmail.com>
2018-03-05 22:39:43 -0300

    Continuação dos exemplos

---
+++ dsi/csharp_01/PrimeiroRenascido/PrimeiroRenascido/Exemplo10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeiroRenascido
+{
+    class Exemplo10
+    {
+        public static void Ternarios()
+        {
+            int a, b;
+            string verdade = "S", falso = "N";
+
+            Console.WriteLine("Digite um número inteiro: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número inteiro: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine
+        }
+    }
+}
